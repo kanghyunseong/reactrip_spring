08e9810218a971537915643eaa38c5a3c35f12e5
Taeho Kim <kth8631@gmail.com>
2026-01-13 16:22:25 +0900

    push test

---
+++ src/main/java/com/kh/reactrip/ReactripApplication.java
@@ -0,0 +1,13 @@
+package com.kh.reactrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactripApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kh/reactrip/auth/model/service/AuthService.java
@@ -0,0 +1,11 @@
+package com.kh.reactrip.auth.model.service;
+
+import java.util.Map;
+
+import com.kh.reactrip.auth.model.dto.MemberLoginDTO;
+
+
+
+public interface AuthService {
+	Map<String, String> login(MemberLoginDTO member);
+}
