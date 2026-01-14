2849a254895dbbd0554097b27bbc2fd54bc5f58e
Taeho Kim <kth8631@gmail.com>
2026-01-14 17:35:17 +0900

    diary

--- src/main/java/com/kh/reactrip/diary/model/dto/DiaryDTO.java
+++ src/main/java/com/kh/reactrip/diary/model/dto/DiaryDTO.java
@@ -16,15 +16,15 @@ import lombok.ToString;
 @ToString
 public class DiaryDTO {
 
-	private int diaryNo;
-	private String diaryTitle;
-	private String diaryContent;
-	private char diaryStatus;
-	private Date createdDate;
-	private Date updatedDate;
-	private int count;
-	private int memberNo;
-	private int scheduleNo;
-	private int travelNo;
+	private int diaryNo;			// 게시글 고유번호
+	private String diaryTitle; 		// 게시글 제목
+	private String diaryContent;	// 게시글 내용
+	private char diaryStatus; 		// 게시글 상태
+	private Date createdDate; 		// 게시글 등록일
+	private Date updatedDate; 		// 게시글 수정일
+	private int count;				// 조회수
+	private int memberNo;			// 회원 고유번호
+	private int scheduleNo;			// 일정 번호
+	private int travelNo;			// 여행지 번호
 	
 }
