c6d7d0840d1f103d8abbf36e3568699a87cbdb06
s2selyn <s2selyn@gmail.com>
2026-01-16 14:31:27 +0900

    refactor: create Locatioin class

--- src/main/java/com/kh/reactrip/place/model/dto/PlaceDTO.java
+++ src/main/java/com/kh/reactrip/place/model/dto/PlaceDTO.java
@@ -8,8 +8,6 @@ public class PlaceDTO {
 	private String travelImage; // TRAVLE_IMAGE, 여행지 사진
 	private String travelStatus; // TRAVLE_STATUS, 여행지 삭제 여부, N = 삭제아님(존재함)
 	private String travelAddress; // TRAVLE_ADDRESS, 주소
-	private Double longitude; // MAP_X, 카카오맵 경도
-	private Double latitude; // MAP_Y, 카카오맵 위도
 	private Integer count; // COUNT, 조회수
 	
 	// 지역 JOIN
--- src/main/java/com/kh/reactrip/place/model/vo/PlaceVO.java
+++ src/main/java/com/kh/reactrip/place/model/vo/PlaceVO.java
@@ -17,8 +17,6 @@ public class PlaceVO {
 	private String travelImage; // TRAVLE_IMAGE, 여행지 사진
 	private String travelStatus; // TRAVLE_STATUS, 여행지 삭제 여부, N = 삭제아님(존재함)
 	private String travelAddress; // TRAVLE_ADDRESS, 주소
-	private Double longitude; // MAP_X, 카카오맵 경도
-	private Double latitude; // MAP_Y, 카카오맵 위도
 	private Integer count; // COUNT, 조회수
 
 }
@@ -32,8 +30,6 @@ public record PlaceVO(
 		String travelImage,
 		String travelStatus,
 		String travelAddress,
-		Double longitude,
-		Double latitude,
 		Integer count
 		) {
 	
