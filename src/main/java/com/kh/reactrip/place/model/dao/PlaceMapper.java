a7a100a0183956adc9c5e4aa5c6c7c2de9d1e84a
s2selyn <s2selyn@gmail.com>
2026-01-21 13:41:10 +0900

    Remove unnecessary compilerArgs

--- src/main/java/com/kh/reactrip/place/model/dao/PlaceMapper.java
+++ src/main/java/com/kh/reactrip/place/model/dao/PlaceMapper.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.RowBounds;
 
 import com.kh.reactrip.place.model.dto.PlaceDTO;
